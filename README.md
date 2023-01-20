43ac1207cc3d322d61ed5c5a1b04fa16fc514777
Anas Abubakar <anasabubakar2015@gmail.com>
2023-01-21 00:53:16 +0100

    My portfolio

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# portfolio1
+ ![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# My Portfolio
+
+> This web page is a mobile veiw and a desktop veiw of my portfolio page.
+> The styling for the desktop veiw was made using CSS media query.
+> The head sections contains the links to external stylesheets and external font.
+> it also contains the title.
+> The body element contains the header, a top section, a work section and, about section.
+> The header contains the logo and a nav element.
+> The nav element has a hambugger menu on the mobile version of the website,
+> The nav elements can be accessed by clicking in the hambuger icon,
+> The feature was added using javaScript DOM. 
+> Animation and transition was added to the header section.
+> The section contains information about the website.
+> The work section generated using javaScript.
+> The section contains cards that shows the works in my portfolio.
+> The cards are positioned using css grid and the card content were positioned using flex box.
+> More details about the project can be accessed using the see project button which is also made using javaScript. 
+> The last section is the contact section which contains a form for contacting the author.
+> The contact form uses javaScript validation. 
+> All sections are positioned using flex box except the work section.
+> The website is then deployed using github pages.
+ 
+
+## Built With
+
+- HTML
+- Css
+- javaScript
+
+## Live Demo
+ https://anasbassi.github.io/portfolio/
+
+## Getting Started
+
+***Go to the a repository on [github](https://github.com/anasbassi/portfolio.git).**
+**Fork the repository to your own github.**
+**Follow the follwing steps to to clone the repository**
+-Click on repository,
+-Click on code,
+-Copy the https link on the page,
+-Open gibash in a folder on your local device,
+-Run the command git clone "the coppied https link" 
+-Go to the folder and you will find the file locally 
+
+
+To get a local copy up and running follow these simple example steps.
+
+## Authors
+
+👤 **Anas Abubakar Bassi**
+
+- GitHub: [@anasbassi](https://github.com/anasbassi)
+- Twitter: [@anasabu92568449](https://twitter.com/anasabu92568449)
+- LinkedIn: [Anas Abubakar](https://linkedin.com/in/anas-abubakar-7b352722b)
+
+## Acknowledgments
+
+- Microverse
+- Toma John Musa
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ js/script.js
@@ -0,0 +1,186 @@
+const hamburger = document.querySelector('.btn-nav');
+const navMenu = document.querySelector('.nav-links');
+const closeBtn = document.querySelector('.close-btn');
+const navLink = document.querySelectorAll('.nav-link');
+const cardsContainer = document.querySelector('.cards-container');
+
+const projectsData = [
+  {
+    name: 'Todo list',
+    description:
+      'The to-do-list app is a software used for monitoring ones daily routine, Built with Html, CSS, javaScript,and webpack .',
+    imageUrl: [
+      './images/projects/todo.jpg',
+      './images/projects/todo.jpg',
+      './images/projects/todo.jpg',
+    ],
+    technologies: ['react', 'redux', 'scss', 'api'],
+    linkLive: 'https://pollution-metrics.netlify.app/',
+    linkSource: 'https://myto-do-list-app.netlify.app/',
+  },
+  {
+    name: 'Bookstore CMS',
+    description:
+      'The Bookstore is a website where the user can display a list of books, add a book by providing a title, an author, and selecting from the categories, and remove a selected book.',
+    imageUrl: [
+      './images/projects/bookstore.webp',
+      './images/projects/bookstore.webp',
+      './images/projects/bookstore.webp',
+    ],
+    technologies: ['react', 'redux', 'scss', 'api'],
+    linkLive: 'https://bookstore-amejid.netlify.app/',
+    linkSource: 'https://github.com/anasbassi/BookStore',
+  },
+  {
+    name: "Space Travelers' Hub",
+    description:
+      'This web application provides commercial and scientific space travel services. The application allows users to book rockets and join selected space missions.',
+    imageUrl: [
+      './images/projects/space.webp',
+      './images/projects/space.webp',
+      './images/projects/space.webp',
+    ],
+    technologies: ['react', 'redux', 'scss', 'api'],
+    linkLive: 'https://space-amejid-stephen.netlify.app/',
+    linkSource: 'https://github.com/anasbassi/Space-Travellers',
+  },
+  {
+    name: 'PokeMon Collection',
+    description:
+      'Pokémon Cards Collection project is the application for showing top 20 pokemon, Users can see details of a specific Pokémon by clicking see-details button Users can give Stars to one or more Cards.Users can comment on pokemon card details.',
+    imageUrl: [
+      './images/projects/pokemon.jpg',
+      './images/projects/pokemon.jpg',
+      './images/projects/pokemon.jpg',
+    ],
+    technologies: ['javascript', 'css', 'html'],
+    linkLive: 'https://saqibjavaiddev.github.io/Pokemon-API/',
+    linkSource: 'https://github.com/saqibjavaiddev/Pokemon-API',
+  },
+  {
+    name: 'African basketball Academy ',
+    description:
+      "The African basketball Academy was built with Html, Css, and, JavaScript. It is a website that belongs to an African Basketball Academy and, it describes the school's curriculum and activities.",
+    imageUrl: [
+      './images/projects/ABA.jpg',
+      './images/projects/ABA.jpg',
+      './images/projects/ABA.jpg',
+    ],
+    technologies: ['javascript', 'css', 'html'],
+    linkLive: 'https://anasbassi.github.io/Capstone-Project-1/',
+    linkSource: 'https://github.com/anasbassi/Capstone-Project-1',
+  },
+  {
+    name: 'Math Magicians',
+    description:
+      'Math-Magicians is a web app for all lovers of mathematics. It is a Single Page Application (SPA) that allows users to make simple calculations and read a random math-related quote.',
+    imageUrl: [
+      './images/projects/math.webp',
+      './images/projects/math.webp',
+      './images/projects/math.webp',
+    ],
+    technologies: ['react', 'scss'],
+    linkLive: 'https://math-magicians-amejid.netlify.app/',
+    linkSource: 'https://github.com/anasbassi/Math-magician',
+  },
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+  for (let i = 0; i < projectsData.length; i += 1) {
+    const projectCard = document.createElement('div');
+    projectCard.classList.add('project-card');
+    const projectCardThumb = document.createElement('div');
+    const projectCardThumbImg = document.createElement('img');
+    const [, , projectImg] = projectsData[i].imageUrl;
+    projectCardThumbImg.src = projectImg;
+    projectCardThumb.classList.add('project-card--thumb');
+    projectCardThumb.appendChild(projectCardThumbImg);
+    projectCard.appendChild(projectCardThumb);
+    const projectCardContent = document.createElement('div');
+    projectCardContent.classList.add('project-card--content');
+    projectCard.appendChild(projectCardContent);
+    const projectTitle = document.createElement('h3');
+    projectTitle.classList.add('project-title');
+    projectTitle.textContent = projectsData[i].name;
+    projectCardContent.appendChild(projectTitle);
+    const projectTools = document.createElement('ul');
+    projectTools.classList.add('project-tools');
+    projectCardContent.appendChild(projectTools);
+
+    for (let j = 0; j < projectsData[i].technologies.length; j += 1) {
+      const projectTool = document.createElement('li');
+      projectTool.textContent = projectsData[i].technologies[j];
+      projectTools.appendChild(projectTool);
+    }
+
+    const projectButton = document.createElement('button');
+    projectButton.classList.add('btn', 'view-project');
+    projectButton.textContent = 'See Project';
+    projectCardContent.appendChild(projectButton);
+    cardsContainer.appendChild(projectCard);
+  }
+});
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.add('mobile-menu');
+  document.body.style.overflow = 'hidden';
+});
+
+closeBtn.addEventListener('click', () => {
+  navMenu.classList.remove('mobile-menu');
+  document.body.style.overflow = 'scroll';
+});
+
+navLink.forEach((link) => link.addEventListener('click', () => {
+  navMenu.classList.remove('mobile-menu');
+}));
+
+document.addEventListener('DOMContentLoaded', () => {
+  const projectDetails = document.querySelectorAll('.view-project');
+  const modalContainer = document.querySelector('.modal-container');
+  const modalImgContainer = document.querySelector('.modal-img-container');
+  const modalTitleLinks = document.querySelector('.modal-title-links');
+  const modalTools = document.querySelector('.modal-tools');
+  const modalDescription = document.querySelector('.modal-desc');
+  const closeModal = document.querySelectorAll('.modal-close');
+  const btnLive = document.querySelector('.btn-live');
+  const btnSource = document.querySelector('.btn-source');
+
+  const modalImgMobile = document.createElement('img');
+  const modalImgDesktop = document.createElement('img');
+  const modalTitle = document.createElement('h3');
+  for (let i = 0; i < projectDetails.length; i += 1) {
+    projectDetails[i].addEventListener('click', () => {
+      modalTools.innerHTML = '';
+      const [imgMobile, imgDesktop] = projectsData[i].imageUrl;
+      modalImgMobile.src = imgMobile;
+      modalImgMobile.classList.add('modal-img', 'modal-img--mobile');
+      modalImgDesktop.src = imgDesktop;
+      modalImgDesktop.classList.add('modal-img', 'modal-img--desktop');
+      modalTitle.textContent = projectsData[i].name;
+      modalTitle.classList.add('modal-title');
+
+      for (let k = 0; k < projectsData[i].technologies.length; k += 1) {
+        const modalTool = document.createElement('li');
+        modalTool.textContent = projectsData[i].technologies[k];
+        modalTools.appendChild(modalTool);
+      }
+
+      modalDescription.textContent = projectsData[i].description;
+      btnLive.href = projectsData[i].linkLive;
+      btnSource.href = projectsData[i].linkSource;
+
+      modalImgContainer.appendChild(modalImgDesktop);
+      modalImgContainer.appendChild(modalImgMobile);
+      modalTitleLinks.appendChild(modalTitle);
+
+      modalContainer.classList.add('open');
+      document.body.style.overflow = 'hidden';
+    });
+  }
+
+  closeModal.forEach((link) => link.addEventListener('click', () => {
+    modalContainer.classList.remove('open');
+    document.body.style.overflow = 'scroll';
+  }));
+});
